a9419e8b9c3654153aadfff9abef534680af7af0
leonsal <leonel.salvadori@gmail.com>
2021-12-19 09:14:11 -0300

    ...

--- imgui/buildgl.go
+++ imgui/buildgl.go
@@ -4,6 +4,6 @@ package imgui
 
 // #cgo CFLAGS:   -I./libs -I./libs/cimgui -I./libs/cimgui/imgui -I./libs/cimplot -I./libs/display
 // #cgo CXXFLAGS: -I./libs -I./libs/cimgui -I./libs/cimgui/imgui -I./libs/cimgui/imgui/backends -I./libs/cimplot -I./libs/cimplot/implot -I./libs/display
-// #cgo linux   LDFLAGS: -L${SRCDIR}/libs -lcimguigl -lGL -lglfw3 -lm -ldl -lX11 -lstdc++
+// #cgo linux   LDFLAGS: -L/usr/local/lib -lcimguigl -lglfw3 -lGL -lm -ldl -lX11 -lstdc++
 // #cgo windows LDFLAGS: -L./libs -lglfw -lstdc++ -lopengl32 -lgdi32 -limm32
 import "C"
