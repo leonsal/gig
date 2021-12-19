abaa8dbe54671349d9b1d98f1effb7ac1455f54d
leonsal <leonel.salvadori@gmail.com>
2021-12-19 10:18:41 -0300

    ...

--- imgui/buildvk.go
+++ imgui/buildvk.go
@@ -4,6 +4,6 @@ package imgui
 
 // #cgo CFLAGS:   -I./libs -I./libs/cimgui -I./libs/cimgui/imgui -I./libs/cimplot -I./libs/display
 // #cgo CXXFLAGS: -I./libs -I./libs/cimgui -I./libs/cimgui/imgui -I./libs/cimgui/imgui/backends -I./libs/cimplot -I./libs/cimplot/implot -I./libs/display
-// #cgo linux   LDFLAGS: -L./libs -lvulkan -lglfw3 -lm -ldl -lX11 -lstdc++
-// #cgo windows LDFLAGS: -L./libs -L${SRCDIR}/libs/vulkan/lib -lglfw3 -lvulkan-1 -lstdc++ -lgdi32 -limm32
+// #cgo linux   LDFLAGS: -L/usr/local/lib -lgigvk -lglfw3 -lvulkan -lm -ldl -lX11 -lstdc++
+// #cgo windows LDFLAGS: -LC:/giglibs -lgigvk -lglfw3 -lvulkan-1 -lstdc++ -lgdi32 -limm32
 import "C"
